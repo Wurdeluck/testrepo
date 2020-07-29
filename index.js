f7de476aa32ebb6e839992f0b000b5f9a3e71a92
wurdeluck <repkin572@gmail.com>
2020-07-29 12:01:37 +0700

    test app

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('app');
+const port = process.env.PORT || 5000
+
+// Only for testing purposes
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Working'
+//   })
+// })
+
+app.listen(port, () => console.log(`Server has been started on port ${port}`))
